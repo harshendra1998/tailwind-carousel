4602a4fe3fc126cc97d560af38dcff2c7737300f
harshendra prajapati <harshendraprajapati@gmail.com>
2022-04-22 11:52:48 +0530

    Added my project

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ export default function Carousel(props) {
     onSwipedRight: () => prev()
   });
 
-  const Arrow = ({ classStyles, func, hideArrowForMobile, timespace }) => <div
-    className={`top-1by2 translate-y absolute pr-4 pl-1 block transition-all duration-100 full-rounded ${hideArrowForMobile && 'hidden'} sm-block ${timespace ? 'opacity-30 hover:opacity-40' : 'opacity-50 hover:opacity-70'} ${classStyles}`} onClick={() => func()}>
+  const Arrow = ({ classStyles, func, hideArrowForMobile, timespace, imageRotate }) => <div
+    className={`top-1by2 translate-y absolute block transition-all duration-100 full-rounded ${hideArrowForMobile && 'hidden'} sm-block ${timespace ? 'opacity-30 hover:opacity-40' : 'opacity-50 hover:opacity-70'} ${classStyles}`} onClick={() => func()}>
     <img
-      className={`w-8 py-0point5 pr-1 ${timespace ? 'bg-gray-800' : 'bg-none'} rounded-full`}
+      className={`w-8 py-0point5 pr-1 ${timespace ? 'bg-gray-800' : 'bg-none'} rounded-full ${imageRotate}`}
       src={ssvg}
       alt="Arrow Icon"
     />
@@ -57,15 +57,15 @@ export default function Carousel(props) {
         <div className="h-4 absolute flex gap-2 bottom-0">
           {props.pageIndicator && props.images.length > 1 && props.images.map((img, index) => (
             <div
-              className={`h-2 w-2 bg-gray-50 rounded-full mb-2 ${index + mode == 0 ? "opacity-90" : "opacity-40"} transition-all duration-200`}
+              className={`h-2 w-2 bg-gray-50 rounded-full mb-2 ${index + mode === 0 ? "opacity-90" : "opacity-40"} transition-all duration-200`}
               onClick={() => setMode(-index)}
             />
           ))}
         </div>
         {/* props.arowVisible && */}
         {props.arrowVisible && props.images.length > 1 && <>
-          <Arrow classStyles='left-0' hideArrowForMobile={props.hideArrowForMobile} timespace={props.timespace} func={prev} />
-          <Arrow classStyles='right-0 rotate-180' hideArrowForMobile={props.hideArrowForMobile} timespace={props.timespace} func={next} /></>}
+          <Arrow classStyles='left-0 pr-4 pl-1' imageRotate={` `} hideArrowForMobile={props.hideArrowForMobile} timespace={props.timespace} func={prev} />
+          <Arrow classStyles='right-0 pl-4 pr-1' imageRotate={`rotate-180`} hideArrowForMobile={props.hideArrowForMobile} timespace={props.timespace} func={next} /></>}
       </div>
     </div>
   );
